5d3857dd2fe3257dd0d2018608b36faff421daa7
Stefan <git@stefn.co.uk>
2024-12-03 22:05:01 +0000

    Remove empty log blog

--- flake.nix
+++ flake.nix
@@ -179,13 +179,6 @@
                 };
               };
               extraConfig = ''
-                {
-                  log {
-                    # format transform `{request>remote_ip} - {request>user_id} [{ts}] "{request>method} {request>uri} {request>proto}" {status} {size} "{request>headers>Referer>[0]}" "{request>headers>User-Agent>[0]}"` {
-                    #   time_format "02/Jan/2006:15:04:05 -0700"
-                    # }
-                  }
-                }
               '';
             };
 
