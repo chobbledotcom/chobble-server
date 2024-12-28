efbc1db8244bdece8dfbdff1a5c5a42a936a816c
Stefan <git@stefn.co.uk>
2024-12-28 21:01:31 +0000

    Ignore assets and robots hits

--- analytics.nix
+++ analytics.nix
@@ -75,6 +75,8 @@ in
                     -format=combined \
                     -site="https://${analyticsHost}" \
                     -exclude 'status:404' \
+                    -exclude 'path:glob:/assets/*' \
+                    -exclude 'path:glob:/robots.txt' \
                     /var/log/caddy/${domain}.log
                 '';
               };
