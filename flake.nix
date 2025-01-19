608ce2badd5acfeb3d10471bf1da72efbc6f617e
Stefan <git@stefn.co.uk>
2025-01-19 19:35:49 +0000

    Remove network=host

--- flake.nix
+++ flake.nix
@@ -217,7 +217,6 @@
                   };
                   extraOptions = [
                     "--pull=newer"
-                    "--network=host"
                   ];
                 };
               };
