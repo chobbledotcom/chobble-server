b5d8485469a898e090fabb34b7357f6647e7cf96
Stefan <git@stefn.co.uk>
2025-04-29 16:37:20 +0100

    Scrap that

--- flake.nix
+++ flake.nix
@@ -262,29 +262,6 @@
               inherit (cfg) sites;
             };
 
-            systemd.timers.update-containers = {
-              timerConfig = {
-                Unit = "update-containers.service";
-                OnCalendar = "Mon 02:00";
-              };
-              wantedBy = [ "timers.target" ];
-            };
-
-            systemd.services.update-containers = {
-              serviceConfig = {
-                Type = "oneshot";
-                ExecStart = lib.getExe (
-                  pkgs.writeShellScriptBin "update-containers" ''
-                    images=$(${pkgs.podman}/bin/podman ps -a --format="{{.Image}}" | sort -u)
-
-                    for image in $images; do
-                      ${pkgs.podman}/bin/podman pull "$image"
-                    done
-                  ''
-                );
-              };
-            };
-
             systemd.services = lib.mkMerge [
               {
                 # Template service for failure notifications
